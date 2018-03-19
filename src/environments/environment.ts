fa40257c1fc27618198e459940b9bfb527e61980
Josh Tate <joshtate04@me.com>
2018-03-19 07:16:54 -0400

    Web animations, enable polyfills

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://time-tracker-api.joshtate.me/api/'
-  // apiUrl: 'http://localhost:3000/api/'
+  // apiUrl: 'https://time-tracker-api.joshtate.me/api/'
+  apiUrl: 'http://localhost:3000/api/'
 };
